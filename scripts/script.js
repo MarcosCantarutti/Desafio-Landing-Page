5e2c8d9226ffff4e0563687f67868ffdebe80f2c
Marcos Cantarutti <marcossandim21@gmail.com>
2021-11-09 13:25:44 -0300

    Last configurations

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+// Menu mobile/desktop
+const navbarBtn = document.querySelector('.menu-toggle')
+const navbarMenu = document.querySelector('.navbar-menu')
+
+navbarBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('show-links')
+})
+
+
+// testimonials slides 
+if(window.innerWidth > 900){
+    document.addEventListener( 'DOMContentLoaded', function() {
+        var splide = new Splide( '.splide',{
+            type   : 'loop',
+            perPage: 3,
+        });
+        splide.mount();
+      } );
+}else{
+    document.addEventListener( 'DOMContentLoaded', function() {
+        var splide = new Splide( '.splide',{
+            type   : 'loop',
+            perPage: 1,
+        });
+        splide.mount();
+      } );
+}
+
+
+  
+  
